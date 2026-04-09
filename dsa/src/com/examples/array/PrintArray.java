9555f612ae410be90813a4d709b762ee7b8c5019
roshanNaik07 <roshannaik202055@gmail.com>
2026-04-09 12:45:39 +0530

    Printing all array elements

---
+++ dsa/src/com/examples/array/PrintArray.java
@@ -0,0 +1,15 @@
+package com.examples.array;
+
+public class PrintArray {
+
+    public static void main(String[] args) {
+
+        String [] names = {"Roshan","Rishi","Sarthak","Sohan","Prajwal"};
+
+        for (String name : names){
+            System.out.println(name);
+        }
+
+    }
+
+}
