00ab73a09fa5481e2554ae04b2cc575e5fdcf674
roshanNaik07 <roshannaik202055@gmail.com>
2026-04-15 12:42:54 +0530

    Declaring a multiDimentional array,  and accessing the elements based on index.

---
+++ dsa/src/com/examples/array/TwoDimentionalArrayDemo.java
@@ -0,0 +1,18 @@
+package com.examples.array;
+
+class TwoDimentionalArrayDemo {
+
+    public static void main(String[] args) {
+
+        String[][] anArray = {
+                {"Roshan", "Rishi", "Ketan"},
+                {"Sarthak", "Manoj"},
+        };
+
+        System.out.println(anArray[0][0] + " " + anArray[0][2]);
+
+        System.out.println(anArray[1][0] + " " + anArray[1][1]);
+
+    }
+
+}
